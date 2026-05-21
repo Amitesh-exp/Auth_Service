fee401f2e2c880244f71ff8bded29bb8e6f4f474
Amitesh-exp <amitesh2878@gmail.com>
2026-05-21 13:09:43 +0530

    Added email and password validation for incoming request

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -29,7 +29,7 @@ const create = async (req, res) => {
 const signIn = async (req, res) => {
     try {
         const response = await userService.signIn(req.body.email, req.body.password);
-         return res.status(200).json({
+        return res.status(200).json({
             success: true,
             message: 'Successfully signed in',
             data: response,
